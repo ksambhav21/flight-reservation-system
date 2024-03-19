a1f6d6b6b8fd9bf3eb8919ac7bbc19ce431ad436
ksambhav2000 <sambhavk@ideyalabs.com>
2024-03-19 09:39:51 +0530

    changed passenger service impl

--- src/main/java/org/ideyalabs/passenger/repository/PassengerRepo.java
+++ src/main/java/org/ideyalabs/passenger/repository/PassengerRepo.java
@@ -3,6 +3,11 @@ package org.ideyalabs.passenger.repository;
 import org.ideyalabs.passenger.entity.Passenger;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PassengerRepo extends JpaRepository<Passenger,Long> {
+
+    Optional<Passenger> findByPassengerId(Long passngerId);
+    Optional<Passenger> findByEmail(String email);
 
 }
